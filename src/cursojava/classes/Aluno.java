307fb8727912513a858b4a32ccc4e6a70c28427b
lucas <lucas@DESKTOP-HEK1RRJ>
2022-12-01 22:03:13 -0300

    alterado a conta bancaria

---
+++ src/cursojava/classes/Aluno.java
@@ -0,0 +1,147 @@
+package cursojava.classes;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import cursojava.constantes.StatusAluno;
+
+/*classe aluno*/
+public class Aluno  extends Pessoa{
+	
+	//atrubito
+	
+	private String dataMatricula;
+	private String nomeEscola;
+	private String serieMatruculado;
+	
+	
+	/*lista de Disciplinas*/
+	private List<Discplina> disciplinas = new ArrayList<Discplina>();
+	
+
+	public List<Discplina> getDisciplinas() {
+		return disciplinas;
+	}
+
+	public void setDisciplinas(List<Discplina> disciplinas) {
+		this.disciplinas = disciplinas;
+	}
+
+	
+
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+
+	public String getSerieMatruculado() {
+		return serieMatruculado;
+	}
+
+	public void setSerieMatruculado(String serieMatruculado) {
+		this.serieMatruculado = serieMatruculado;
+	}
+	
+	
+	/*contrutor*/
+	public Aluno() {
+		
+	}
+	
+	/*Metodo que retorna  amedia do aluno*/
+	public double getMediaNota() {
+		double somaTotal = 0.0;
+		for (Discplina discplina : disciplinas) {
+			somaTotal += discplina.getNota();
+		}
+		return somaTotal / disciplinas.size();
+	}
+	// metodo simples para saber se o Aluno esta Aprovado ou não com boolean
+	public boolean getAlunoAprovado() {
+		double media = this.getMediaNota();
+		
+		if(media >=70) {
+			return true;
+		}else {
+			return false;
+		}
+	
+		
+	}
+	// metodo para saber se o Aluno esta Aprovado ou não
+	public String  getAlunoAprovado2() {
+		double media = this.getMediaNota();
+		
+		if(media >=50) {
+			if(media >=70) {
+				return StatusAluno.APROVADO;
+			}else {
+				return StatusAluno.RECUPERACAO;
+			}
+		}else {
+			return StatusAluno.REPROVADO;
+		}
+			
+		}
+
+	@Override
+	public String toString() {
+		return "Aluno [dataMatricula=" + dataMatricula + ", nomeEscola=" + nomeEscola + ", serieMatruculado="
+				+ serieMatruculado + ", disciplinas=" + disciplinas + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(dataMatricula, disciplinas, nomeEscola, serieMatruculado);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		return Objects.equals(dataMatricula, other.dataMatricula) && Objects.equals(disciplinas, other.disciplinas)
+				&& Objects.equals(nomeEscola, other.nomeEscola)
+				&& Objects.equals(serieMatruculado, other.serieMatruculado);
+	}
+	@Override
+	public boolean PessoaMairoIdade() {
+		// TODO Auto-generated method stub
+		return super.PessoaMairoIdade();
+	}
+	@Override
+	public String msgMairoIdade() {
+		// TODO Auto-generated method stub
+		return super.msgMairoIdade();
+	}
+
+	@Override
+	public double Salario() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+			
+		
+	
+
+	
+	
+	
+
+}
---
+++ src/cursojava/principioderesponsabilidade/TesteContaBancaria.java
@@ -0,0 +1,27 @@
+package cursojava.principioderesponsabilidade;
+
+public class TesteContaBancaria {
+	
+	
+	public static void main(String[] args) {
+		
+		ContaBancaria bancaria = new ContaBancaria();
+		bancaria.setDescricao("ocnta do Antonia mesquita");
+		
+		System.out.println(bancaria);
+		
+	//	bancaria.Diminuir100Reais();
+	//	System.out.println(bancaria);
+		
+    	bancaria.SacarDinheiro(800);
+		System.out.println(bancaria);
+		
+		bancaria.DepositarDineheiro(1500);
+		System.out.println(bancaria);
+		
+		System.out.println("conta gorda!");
+		
+		
+	}
+
+}
